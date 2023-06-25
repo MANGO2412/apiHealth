ad7692646bc7605212299af5be6ffa6fc8fec514
Manuel De Jes�s G�mez P�rez <0321101422@ut-tijuana.edu.mx>
2023-06-25 08:32:02 -0700

    v1

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import 'dotenv/config';
+import DB from "./db.js";
+import cors from 'cors';
+
+const app=express();
+app.use(cors())
+
+//db class 
+let mongo=new DB(process.env.MONGOSH,process.env.ATLAS) 
+
+app.get('/',async (req,res)=>{
+    try {
+        const result = await mongo.get('Movie');
+        res.send(result).status(200);
+      } catch (error) {
+        console.error('Error retrieving data:', error);
+        res.sendStatus(500);
+      }
+})
+
+export default  app;
---
+++ src/db.js
@@ -0,0 +1,94 @@
+import  {MongoClient} from "mongodb";
+import 'dotenv/config';
+
+
+export default class DB{
+    constructor(Mongosh,Atlas){
+        // this.connectionString=process.env.MONGOSH || "";
+        this.clientLocal=new MongoClient(Mongosh,{serverSelectionTimeoutMS:1000});
+        this.clientAtlas=new MongoClient(Atlas)
+
+        //code to mysql here
+        }
+     
+    //this method validate the conection of databases
+    connectionDBS=async ()=>{
+        let dbs={}
+
+        console.time('connect')
+        //check if there is  connection to Monogosh
+        try {
+            let conn=await this.clientLocal.connect()
+            console.timeEnd('connect')
+            dbs['mongosh']= conn.db('cinema');  
+        } catch (error) {
+            console.log("there is any error with mongosh: ",error)
+            console.timeEnd('connect')
+            dbs['mongosh']= null
+        }
+
+
+        //check if there is connection to Atlas
+        try{
+            let conn=await this.clientAtlas.connect()
+            dbs['atlas']= conn.db('cinema');
+        }catch(error){
+            console.log("there is any error with atlas: ",error)
+            dbs['atlas']=null
+        }
+
+        return dbs;
+
+    }
+
+    validateQuery= async (db,collec,command,codition=null)=>{
+        if(db.mongosh != null && db.atlas != null){
+          return {
+            'resultMongosh': await this.query(db.atlas,collec,command,codition),
+            'resultAtlas':await this.query(db.mongosh,collec,command,codition),
+            'error':null
+          } 
+        }else if(db.mongosh != null){
+            return {
+                'resultMongosh': await this.query(db.mongosh,collec,command,codition),
+                'resultAtlas':null,
+                'error':null
+            } 
+
+        }else if(db.atlas != null){
+            return {
+                'resultMongosh': null,
+                'resultAtlas':await this.query(db.atlas,collec,command,codition),
+                'error':null
+              } 
+        }
+
+        return {"error":"error 500"}
+    }
+
+
+    query=async (db,collec,command,codition)=>{
+       if(command=="find"){
+         let coleccion = await db.collection(collec)
+         let result= await coleccion[command]({})
+          return result.toArray();
+       }
+
+       return ''
+    }
+
+
+    //method to crud in the databases
+    get= async (collec)=>{
+       let dbs= await this.connectionDBS();
+       let result= await this.validateQuery(dbs,collec,'find');
+
+       if(result['resultMongosh'] != null)
+           return result['resultMongosh']
+        else if ( result['resultAtlas'] != null)
+           return result['resultAtlas']
+        
+       return result['error'];
+    }
+
+}
