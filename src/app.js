4e4b61d723b2b11c7c1648d562f3cc088c2b07bd
MANGO2412 <manuelgom3z22052003@gmail.com>
2023-06-26 22:37:10 -0700

    beta4

--- src/app.js
+++ src/app.js
@@ -4,16 +4,41 @@ import DB from "./db.js";
 import cors from 'cors';
 import routes from './routes/index.js'
 
+//mqtt
+import * as mqtt from 'mqtt'
+
+
+
 //configuration
 const app=express();
 app.use(cors())
 app.use(routes)
+let mongo=new DB(process.env.MONGOSH,process.env.ATLAS)
 
-let mongo=new DB(process.env.MONGOSH,process.env.ATLAS) 
+//configurstion to mqtt client
+let client=mqtt.connect('tcp://broker.hivemq.com'+':1883')
+const topic="/UTT/0321101422/send/onevalue";
+
+client.on('connect',()=>{
+  console.log('connected')
+  client.subscribe([topic],()=>{
+    console.log(`Subscribe to topic ${topic}`)
+  })
+})
+
+client.on('error', (error) => {
+  console.error('connection failed', error)
+})
+
+//conection
+client.on('message',(topic,payload)=>{
+ console.log('Received Message',topic,payload.toString())
+})
 
 
 //page to welcome api
 app.get('/',(req,res)=>{
+   //connection
    res.send("hello world")
 })
 
